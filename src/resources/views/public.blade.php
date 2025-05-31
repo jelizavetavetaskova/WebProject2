4b504986100a4f27b0ef3b5d1e62a4c994534183
Jeļizaveta Vetaškova <s24vetajeli@venta.lv>
2025-05-31 22:00:36 +0300

    Pievienota ikona

--- src/resources/views/public.blade.php
+++ src/resources/views/public.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ $title }}</title>
         <meta name="description" content="Jeļizavetas Vetaškovas 2. praktiskais darbs - dziesmu lapa">
+        <link rel="icon" href="{{ asset('favicon.ico') }}">
         @viteReactRefresh
         @vite('resources/css/app.css')
         @vite('resources/js/index.jsx')
